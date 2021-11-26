095ef906a8ab60b4683448be8382699673a12a32
Francilene Silva <francilenesilva.fps10@gmail.com>
2021-11-26 11:41:28 +0000

    entendendo variáveis, declaração e seus tipos

--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,30 @@
-fun main() {
-    println("Bem vindo ao Bytebank")
+fun main(args : Array<String>){
+    // DECLARANDO VARIÁVEIS
+    var usuario = "Francilene" // var declara variáveis mutáveis
+    val pi = 3.14 // val declara variáveis imutáveis
+
+    var valor1 = 5
+    var valor2 = 4
+    var total = valor1 + valor2
+
+    // imprimindo valores
+    println(usuario)
+    println(pi)
+    println(total)
+    // imprimindo valor da variável dentro de uma string
+    print("o total da soma é: $total")
+
+    // TIPOS DE VARIÁVEIS
+    // kotlin é dinamicamente tipada, não precisa definir o tipo da variável
+    var numero: Double = 3.5;
+    var numeroFloat: Float = 5.3f
+    var number: Int; // especifique o tipo caso a variável não for inicializada
+    var texto: String;
+    var numeroLong: Long = 10L
+    var result: Boolean = true
+
+    texto  = "Francilene é uma estudante"
+
+    println()
+
 }
-
-
-
-
