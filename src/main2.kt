01a9a4d232b4ca450b67fdd5699d62cc06f921b3
Francilene Silva <francilenesilva.fps10@gmail.com>
2021-11-26 21:00:11 +0000

    aprendendo arrays

---
+++ src/main2.kt
@@ -0,0 +1,16 @@
+fun main(args : Array<String>){
+
+    // declarando arrays
+    var nomes = arrayOf("Francilene", "Pedro", "Nicholas");
+    println(nomes[0])
+    nomes[0] = "Renata"
+    println(nomes[0])
+
+    // não tem como adicionar valor ao array criado
+    //nomes[3] = "Renata"
+
+    var numeros = arrayOf(20, 30, 40)
+    var Numeros2 = intArrayOf(50, 60, 70)
+    println(numeros[2])
+
+}
